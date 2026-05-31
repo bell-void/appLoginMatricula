e746e306213fc5c83c5df4125bd1ffaf417421e6
Dayron <pescobarmm@gmail.com>
2026-05-31 11:43:08 -0500

    separe CSS de welcome.blade.php a public/css/welcome.css

--- database/seeders/AulaSeeder.php
+++ database/seeders/AulaSeeder.php
@@ -10,14 +10,14 @@ class AulaSeeder extends Seeder
     public function run(): void
     {
         $aulas = [
-            ['nombre_aula' => 'Aula 101',       'capacidad' => 30,  'ubicacion' => 'Piso 1 - Pabellón A'],
-            ['nombre_aula' => 'Aula 102',       'capacidad' => 30,  'ubicacion' => 'Piso 1 - Pabellón A'],
-            ['nombre_aula' => 'Aula 201',       'capacidad' => 40,  'ubicacion' => 'Piso 2 - Pabellón A'],
-            ['nombre_aula' => 'Aula 202',       'capacidad' => 40,  'ubicacion' => 'Piso 2 - Pabellón A'],
-            ['nombre_aula' => 'Lab Informática A', 'capacidad' => 25, 'ubicacion' => 'Piso 1 - Pabellón B'],
-            ['nombre_aula' => 'Lab Informática B', 'capacidad' => 25, 'ubicacion' => 'Piso 1 - Pabellón B'],
-            ['nombre_aula' => 'Auditorio',      'capacidad' => 200, 'ubicacion' => 'Planta Baja'],
-            ['nombre_aula' => 'Sala de Reuniones', 'capacidad' => 15, 'ubicacion' => 'Piso 3 - Rectorado'],
+            ['nombre_aula' => 'Aula 101',          'capacidad' => 30,  'ubicacion' => 'Piso 1 - Pabellón A'],
+            ['nombre_aula' => 'Aula 102',          'capacidad' => 30,  'ubicacion' => 'Piso 1 - Pabellón A'],
+            ['nombre_aula' => 'Aula 201',          'capacidad' => 40,  'ubicacion' => 'Piso 2 - Pabellón A'],
+            ['nombre_aula' => 'Aula 202',          'capacidad' => 40,  'ubicacion' => 'Piso 2 - Pabellón A'],
+            ['nombre_aula' => 'Lab Informática A', 'capacidad' => 25,  'ubicacion' => 'Piso 1 - Pabellón B'],
+            ['nombre_aula' => 'Lab Informática B', 'capacidad' => 25,  'ubicacion' => 'Piso 1 - Pabellón B'],
+            ['nombre_aula' => 'Auditorio',         'capacidad' => 200, 'ubicacion' => 'Planta Baja'],
+            ['nombre_aula' => 'Sala de Reuniones', 'capacidad' => 15,  'ubicacion' => 'Piso 3 - Rectorado'],
         ];
 
         foreach ($aulas as $aula) {
